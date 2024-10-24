9480d56899d6df36f7202776096294c8b6601e51
IlhamArifinnn <arifinn2208@gmail.com>
2024-10-24 22:52:18 +0700

    add HewanController

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AnimalController;
+use App\Http\Controllers\HewanController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,3 +14,10 @@ Route::get('/animals', [AnimalController::class, 'index']);
 Route::post('/animals', [AnimalController::class, 'store']);
 Route::put('/animals/{id}', [AnimalController::class, 'update']);
 Route::delete('/animals/{id}', [AnimalController::class, 'destroy']);
+
+
+
+Route::get('/hewans', [HewanController::class, 'index']);
+Route::post('/hewans', [HewanController::class, 'store']);
+Route::patch('/hewans/{id}', [HewanController::class, 'update']);
+Route::delete('/hewans/{id}', [HewanController::class, 'destroy']);
