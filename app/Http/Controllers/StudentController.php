cffa2bbeb2280c442b21e43f8fc53fff15a49b41
IlhamArifinnn <arifinn2208@gmail.com>
2024-11-09 16:24:15 +0700

    upload praktikum week7

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Student;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class StudentController extends Controller
+{
+    public function index()
+    {
+        $students = Student::all();
+
+        if ($students) {
+            $data = [
+                'message' => 'Get all students',
+                'data' => $students,
+            ];
+        } else {
+            $data = [
+                'message' => 'Student is empty',
+            ];
+        }
+
+        return response()->json($data, 200);
+    }
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'nama' => 'required',
+            'nim' => 'numeric|required',
+            'email' => 'email|required',
+            'jurusan' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'message' => 'Validation errors',
+                'errors' => $validator->errors(),
+            ], 422);
+        }
+
+        $student = Student::create($request->all());
+
+        $data = [
+            'message' => 'Student is created successfully',
+            'data' => $student,
+        ];
+
+        return response()->json($data, 201);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $student = Student::find($id);
+
+        if ($student) {
+            $input = [
+                'nama' => $request->nama ?? $student->nama,
+                'nim' => $request->nim ?? $student->nim,
+                'email' => $request->email ?? $student->email,
+                'jurusan' => $request->jurusan ?? $student->jurusan,
+            ];
+
+            $student->update($input);
+
+            $data = [
+                'message' => 'Student is updated',
+                'data' => $student,
+            ];
+
+            return response()->json($data, 200);
+        } else {
+            $data = [
+                'message' => 'Student not found',
+            ];
+
+            return response()->json($data, 404);
+        }
+    }
+
+    public function destroy($id)
+    {
+        $student = Student::find($id);
+
+        if ($student) {
+            $student->delete();
+
+            $data = [
+                'message' => 'Student is deleted',
+            ];
+
+            return response()->json($data, 200);
+        } else {
+            $data = [
+                'message' => 'Student not found',
+            ];
+
+            return response()->json($data, 404);
+        }
+    }
+
+    public function show($id)
+    {
+        $student = Student::find($id);
+
+        if ($student) {
+            $data = [
+                'message' => 'Get detail student',
+                'data' => $student
+            ];
+
+            return response()->json($data, 200);
+        } else {
+            $data = [
+                'message' => 'Student not found',
+            ];
+
+            return response()->json($data, 404);
+        }
+    }
+}
