f66e482d276a775ce4877a69d861a3c8c2ba2e26
IlhamArifinnn <arifinn2208@gmail.com>
2024-12-07 07:15:51 +0700

    upload praktikum week11: Asynchronous Programming: update task

--- task/task.js
+++ task/task.js
@@ -2,21 +2,26 @@
  * Fungsi untuk menampilkan hasil download
  * @param {string} result - Nama file yang didownload
  */
-function showDownload(result) {
-  console.log("Download selesai");
-  console.log("Hasil Download: " + result);
-}
+const showDownload = (result) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log("Download selesai");
+      console.log("Hasil Download: " + result);
+    });
+  });
+};
 
 /**
  * Fungsi untuk download file
- * @param {function} callback - Function callback show
+ * @param {const} callback - = Function => callback show
  */
-function download(callShowDownload) {
+const download = (callShowDownload) => {
+  console.log("Download dimulai...");
   setTimeout(function () {
     const result = "windows-10.exe";
     callShowDownload(result);
   }, 3000);
-}
+};
 
 download(showDownload);
 
